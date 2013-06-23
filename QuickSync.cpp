4d5fa469179291a60301fce8c592899dfee0101d
Hendrik Leppkes <h.leppkes@gmail.com>
2013-06-23 08:19:02 +0200

    Don't use the AVC FrameConstructor for Annex B

--- QuickSync.cpp
+++ QuickSync.cpp
@@ -196,10 +196,10 @@ HRESULT CQuickSync::HandleSubType(const AM_MEDIA_TYPE* mtIn, FOURCC fourCC, mfxV
 
         videoParams.mfx.CodecId = MFX_CODEC_AVC;
         // Note: CAVCFrameConstructor can handle both H264 stream types but it can't handle fragment streams (e.g. live TV)
-//        pFrameConstructor = ((fourCC == FOURCC_avc1) || (fourCC == FOURCC_AVC1) || (fourCC == FOURCC_CCV1)) ?
-//            new CAVCFrameConstructor :
-//            new CFrameConstructor;
-        pFrameConstructor = new CAVCFrameConstructor;
+        pFrameConstructor = ((fourCC == FOURCC_avc1) || (fourCC == FOURCC_AVC1) || (fourCC == FOURCC_CCV1)) ?
+            new CAVCFrameConstructor :
+            new CFrameConstructor;
+        //pFrameConstructor = new CAVCFrameConstructor;
     }
     else
     {
